1619c29da828447b84d908c7c1ca4d8ff2a9ee64
Steve Nyanumba <reach@stevenyanumba.com>
2022-02-28 11:37:50 +0300

    Database Part almost complete

---
+++ database/migrations/2022_02_27_224126_create_client_service_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateClientServicePivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_service', function (Blueprint $table) {
+            $table->unsignedBigInteger('client_id')->index();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->unsignedBigInteger('service_id')->index();
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
+            $table->primary(['client_id', 'service_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_service');
+    }
+}
